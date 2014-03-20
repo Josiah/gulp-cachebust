4d0f357383e3ba54059ddb66edc19e070c1f4a7d
Josiah <josiah@jjs.id.au>
2014-03-20 17:13:39 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# [gulp](https://github.com/wearefractal/gulp)-cachebust
+
+> Generates checksums and renames references to files
+
+Useful for cachebusting
+
+## Install
+
+Install with [npm](https://npmjs.org/package/gulp-cachebust)
+
+```
+npm install --save-dev gulp-cachebust
+```
+
+
+## Example
+
+```js
+var gulp = require('gulp');
+var CacheBuster = require('gulp-cachebust');
+
+var cachebust = new CacheBuster();
+
+gulp.task('build-css', function () {
+    return gulp.src('styles/*.css')
+        // Awesome css stuff
+        .pipe(cachebust.resources())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build-html', ['build-css'], function () {
+    return gulp.src('templates/*')
+        // Awesome html stuff
+        .pipe(cachebust.references())
+        .pipe(gulp.dest('dist'));
+});
+```
+
+
+## API
+
+### new CacheBuster(options)
+
+#### options.checksumLength
+
+*Optional*
+
+Type: `Number`
+Default: 8
+
+### CacheBuster.resources()
+
+Renames and collects resources according to their MD5 checksum.
+
+### CacheBuster.references()
+
+Rewrites references to resources which have been renamed according to their MD5
+checksum.
+
+## License
+
+MIT © [Josiah Truasheim](//github.com/Josiah)
---
+++ index.js
@@ -0,0 +1,112 @@
+var crypto = require('crypto');
+var through2 = require('through2');
+var gutil = require('gulp-util');
+var path = require('path');
+var PluginError = gutil.PluginError;
+
+module.exports = CacheBuster;
+
+function CacheBuster(options) {
+    if (!(this instanceof CacheBuster)) {
+        return new CacheBuster(options);
+    }
+
+    this.checksumLength = (options && options.checksumLength) || 8;
+    this.mappings = {};
+}
+
+CacheBuster.prototype.getChecksum = function getChecksum(file) {
+    var hash = crypto.createHash('md5');
+
+    if (file.isNull()) {
+        return; 
+    }
+
+    if (file.isStream()) {
+        file.pipe(hash);
+    }
+
+    if (file.isBuffer()) {
+        hash.end(file.contents);
+    }
+
+    return hash.read().toString('hex').substr(0, this.checksumLength);
+}
+
+CacheBuster.prototype.getBustedPath = function getBustedPath(file) {
+    var checksum = this.getChecksum(file);
+
+    if (!checksum) {
+        return file.path;
+    }
+
+    var extname = path.extname(file.path);
+    var basename = path.basename(file.path, extname);
+    var dirname = path.dirname(file.path);
+
+    return path.join(dirname, basename + '.' + checksum + extname);
+};
+
+CacheBuster.prototype.getRelativeMappings = function getRelativeMappings() {
+    var mappings = [];
+
+    for (var original in this.mappings) {
+        var cachebusted = this.mappings[original];
+
+        mappings.push({original: original, cachebusted: cachebusted});
+    }
+
+    return mappings;
+};
+
+CacheBuster.prototype.resources = function resources() {
+    var cachebuster = this;
+
+    return through2.obj(function transform(file, encoding, callback) {
+        var bustedPath = cachebuster.getBustedPath(file);
+
+        if (file.path === bustedPath) {
+            this.push(file);
+            return callback();
+        }
+
+        var original = file.relative;
+        file.path = bustedPath;
+
+        cachebuster.mappings[original] = file.relative;
+
+        this.push(file);
+        return callback();
+    });
+};
+
+CacheBuster.prototype.references = function references() {
+    var cachebuster = this;
+
+    return through2.obj(function transform(file, encoding, callback) {
+        if (file.isNull()) {
+            this.push(file);
+            return callback();
+        }
+
+        if (!file.isBuffer()) {
+            this.emit('error', new PluginError('gulp-cachebust', 'Non buffer files are not supported for reference rewrites.'));
+            this.push(file);
+            return callback();
+        }
+
+        var contents = file.contents.toString(encoding);
+
+        var mappings = cachebuster.getRelativeMappings(file.path);
+        for (var i=0; i < mappings.length; i++) {
+            var original = mappings[i].original;
+            var cachebusted = mappings[i].cachebusted;
+
+            contents = contents.replace(original, cachebusted);
+        }
+
+        file.contents = new Buffer(contents, encoding);
+        this.push(file);
+        return callback();
+    });
+};
